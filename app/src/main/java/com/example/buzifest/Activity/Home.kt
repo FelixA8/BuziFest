3bf01a2176e2191cb32d59225bed40db1dcc619c
johansenmarlee21 <johansenmarlee21@gmail.com>
2024-06-01 18:39:31 +0700

    home front end

--- app/src/main/java/com/example/buzifest/Activity/Home.kt
+++ app/src/main/java/com/example/buzifest/Activity/Home.kt
@@ -2,11 +2,47 @@ package com.example.buzifest.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.buzifest.Fragment.ChatFragment
+import com.example.buzifest.Fragment.HomeFragment
+import com.example.buzifest.Fragment.PortofolioFragment
+import com.example.buzifest.Fragment.ShopListFragment
 import com.example.buzifest.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Home : AppCompatActivity() {
+
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottomNav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.bottonNav_portfolio -> {
+                    loadFragment(PortofolioFragment())
+                    true
+                }
+                R.id.bottomNav_shopList -> {
+                    loadFragment(ShopListFragment())
+                    true
+                }
+                R.id.bottonNav_chat -> {
+                    loadFragment(ChatFragment())
+                    true
+                }
+                else -> {false}
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
