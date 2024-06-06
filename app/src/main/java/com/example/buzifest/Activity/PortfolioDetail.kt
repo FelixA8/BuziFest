22b0e66a20f7d6cbf1f9eea0a50b6de8221200d4
FelixAnderson <felix.anderson1504@gmail.com>
2024-06-06 17:22:40 +0700

    Added GMAps

--- app/src/main/java/com/example/buzifest/Activity/PortfolioDetail.kt
+++ app/src/main/java/com/example/buzifest/Activity/PortfolioDetail.kt
@@ -1,12 +1,79 @@
 package com.example.buzifest.Activity
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.example.buzifest.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.Task
 
-class PortfolioDetail : AppCompatActivity() {
+class PortfolioDetail : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var myMap: GoogleMap
+    private lateinit var currLocation: Location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_portfolio_detail)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+    }
+    fun getLastLocation(){
+        //Get the permission of the device and set the lat and lng of the location.
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 1)
+
+            return
+        }
+        //If permission granted, then set the location.
+        var task: Task<Location> = fusedLocationProviderClient.getLastLocation()
+        task.addOnSuccessListener { location: Location? ->
+            if(location != null) {
+                currLocation = location
+            } else {
+                currLocation.longitude = 106.78113
+                currLocation.latitude = -6.20201
+            }
+            var mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.detail_maps) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 1) {
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation()
+            } else {
+                Toast.makeText(this, "Location Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    //Render the google maps
+    override fun onMapReady(p0: GoogleMap) {
+        myMap = p0
+        var myLocation = LatLng(currLocation.latitude, currLocation.longitude)
+        var cameraPosition = CameraPosition.builder().target(myLocation).zoom(20.0F).build()
+        myMap.addMarker(MarkerOptions().position(myLocation).title("Location"))
+        myMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+        myMap.isBuildingsEnabled = true
     }
 }
